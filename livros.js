35d1cbf9998589f3d9a6a8e51b17f9ed50429678
André Luís Bordignon <albordignon@gmail.com>
2022-05-28 10:59:36 -0300

    .

---
+++ livros.js
@@ -0,0 +1,37 @@
+livros = [
+  {
+    nome: "A descoberta do mundo",
+    autor: "Clarice Lispector",
+    paginas: "480",
+    imagem:"https://m.media-amazon.com/images/I/61iz3UgVyJL.jpg"
+
+  },
+  {
+    nome: "Gênero e desigualdades",
+    autor: "Flávia Biroli",
+    paginas: "210",
+    imagem:"https://boitempo-img.f1cdn.com.br/resizer/view/900/900/false/true/542.jpg"
+  },
+  {
+    nome: "A ditadura é assim",
+    autor: " Equipo Plantel",
+    paginas: "52",
+    imagem:"https://martinsfontespaulista.vteximg.com.br/arquivos/ids/200732-800-800/767022_ampliada.jpg?v=637266030488030000"
+  },
+  {
+    nome: "Pequeno Manual Antirracista",
+    autor: "Djamila Ribeiro",
+    paginas: "136",
+    imagem:"https://images-na.ssl-images-amazon.com/images/I/71D3cAdm0GL.jpg"
+  }, 
+  {
+    nome: "Homofobia - História e crítica de um preconceito",
+    autor: "Daniel Borrillo",
+    paginas: "144",
+    imagem:"https://images-na.ssl-images-amazon.com/images/I/91AzPtbpRuL.jpg"
+  }
+
+
+
+  
+]
