9f039887b5c668044f2d7dfca1cfa431499be291
André Luís Bordignon <albordignon@gmail.com>
2022-06-15 22:01:12 -0300

    Incluindo Firebase

--- applivros/src/context/BooksProvider.js
+++ applivros/src/context/BooksProvider.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
 import { db } from '../data/env';
 export const BooksContext = React.createContext();
 
@@ -8,64 +8,72 @@ const BooksProvider = (props) => {
   const [livros, setLivros] = useState([]);
 
   useEffect(() => {
-
-    async function fetchData(){
-      const querySnapshot = await getDocs(collection(db, "users"));
+    async function fetchData() {
+      const querySnapshot = await getDocs(collection(db, "books"));
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
+        const novoLivro = {
+          id: doc.id,
+          nome: doc.data().nome,
+          autor: doc.data().autor,
+          paginas: doc.data().paginas,
+          imagem: doc.data().imagem,
+        };
+        const livrosAux = [...livros];
+        livrosAux.push(novoLivro);
+        setLivros(livrosAux);
       });
     }
-
     fetchData();
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    setLivros(
-      [
-        {
-          id: 0,
-          nome: "A descoberta do mundo",
-          autor: "Clarice Lispector",
-          paginas: "480",
-          imagem: "https://m.media-amazon.com/images/I/61iz3UgVyJL.jpg"
+  //   setLivros(
+  //     [
+  //       {
+  //         id: 0,
+  //         nome: "A descoberta do mundo",
+  //         autor: "Clarice Lispector",
+  //         paginas: "480",
+  //         imagem: "https://m.media-amazon.com/images/I/61iz3UgVyJL.jpg"
 
-        },
-        {
-          id: 1,
-          nome: "Gênero e desigualdades",
-          autor: "Flávia Biroli",
-          paginas: "210",
-          imagem: "https://boitempo-img.f1cdn.com.br/resizer/view/900/900/false/true/542.jpg"
-        },
-        {
-          id: 2,
-          nome: "A ditadura é assim",
-          autor: " Equipo Plantel",
-          paginas: "52",
-          imagem: "https://martinsfontespaulista.vteximg.com.br/arquivos/ids/200732-800-800/767022_ampliada.jpg?v=637266030488030000"
-        },
-        {
-          id: 3,
-          nome: "Pequeno Manual Antirracista",
-          autor: "Djamila Ribeiro",
-          paginas: "136",
-          imagem: "https://images-na.ssl-images-amazon.com/images/I/71D3cAdm0GL.jpg"
-        },
-        {
-          id: 4,
-          nome: "Homofobia - História e crítica de um preconceito",
-          autor: "Daniel Borrillo",
-          paginas: "144",
-          imagem: "https://images-na.ssl-images-amazon.com/images/I/91AzPtbpRuL.jpg"
-        }
-      ]
-    );
-  }, []);
+  //       },
+  //       {
+  //         id: 1,
+  //         nome: "Gênero e desigualdades",
+  //         autor: "Flávia Biroli",
+  //         paginas: "210",
+  //         imagem: "https://boitempo-img.f1cdn.com.br/resizer/view/900/900/false/true/542.jpg"
+  //       },
+  //       {
+  //         id: 2,
+  //         nome: "A ditadura é assim",
+  //         autor: " Equipo Plantel",
+  //         paginas: "52",
+  //         imagem: "https://martinsfontespaulista.vteximg.com.br/arquivos/ids/200732-800-800/767022_ampliada.jpg?v=637266030488030000"
+  //       },
+  //       {
+  //         id: 3,
+  //         nome: "Pequeno Manual Antirracista",
+  //         autor: "Djamila Ribeiro",
+  //         paginas: "136",
+  //         imagem: "https://images-na.ssl-images-amazon.com/images/I/71D3cAdm0GL.jpg"
+  //       },
+  //       {
+  //         id: 4,
+  //         nome: "Homofobia - História e crítica de um preconceito",
+  //         autor: "Daniel Borrillo",
+  //         paginas: "144",
+  //         imagem: "https://images-na.ssl-images-amazon.com/images/I/91AzPtbpRuL.jpg"
+  //       }
+  //     ]
+  //   );
+  // }, []);
 
   const navigate = useNavigate();
 
-  const incluirLivro = (event) => {
+  const incluirLivro = async (event) => {
     event.preventDefault();
     console.log('Inclue livro');
     const id = livros.length;
@@ -79,6 +87,17 @@ const BooksProvider = (props) => {
     const livrosAux = [...livros];
     livrosAux.push(novoLivro);
     setLivros(livrosAux);
+
+    //gravarLivroDB();
+    console.log(db);
+    await setDoc(doc(db, "books", id.toString()), {
+      id: id,
+      nome: event.target.nome.value,
+      autor: event.target.autor.value,
+      paginas: event.target.paginas.value,
+      imagem: event.target.imagem.value,
+    });
+
     navigate('/');
   }
 
--- applivros/src/data/env.js
+++ applivros/src/data/env.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAn6bViRobzNBEvfANu8eFEqBji3_KE7E",
-  authDomain: "cestascooperflorabarao-dev.firebaseapp.com",
-  databaseURL: "https://cestascooperflorabarao-dev-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cestascooperflorabarao-dev",
-  storageBucket: "cestascooperflorabarao-dev.appspot.com",
-  messagingSenderId: "384118273017",
-  appId: "1:384118273017:web:046a319cf0b479f7d2c328"
+  apiKey: "AIzaSyAJlG63CO_RESLEa2LJsG9yOvGenb79eGM",
+  authDomain: "books-19d2d.firebaseapp.com",
+  projectId: "books-19d2d",
+  storageBucket: "books-19d2d.appspot.com",
+  messagingSenderId: "1019171676981",
+  appId: "1:1019171676981:web:06904f78de983e8907227c"
 };
 
 // Initialize Firebase
