c98f8dfd68d0db25372276f0043d0bf523664e7b
Andre Luis Bordignon <albordignon@gmail.com>
2022-10-20 10:56:20 -0300

    Atualização dos livros

--- livros.js
+++ livros.js
@@ -4,35 +4,42 @@ const books = [
     name: "A descoberta do mundo",
     author: "Clarice Lispector",
     pages: 480,
-    image:"https://m.media-amazon.com/images/I/61iz3UgVyJL.jpg"
+    image: "https://m.media-amazon.com/images/I/61iz3UgVyJL.jpg"
 
   },
   {
- id: 1,
+    id: 1,
     name: "Gênero e desigualdades",
     author: "Flávia Biroli",
     pages: 210,
-    image:"https://boitempo-img.f1cdn.com.br/resizer/view/900/900/false/true/542.jpg"
+    image: "https://boitempo-img.f1cdn.com.br/resizer/view/900/900/false/true/542.jpg"
   },
   {
- id: 2,
+    id: 2,
     name: "A ditadura é assim",
     author: " Equipo Plantel",
     pages: 52,
-    image:"https://martinsfontespaulista.vteximg.com.br/arquivos/ids/200732-800-800/767022_ampliada.jpg?v=637266030488030000"
+    image: "https://martinsfontespaulista.vteximg.com.br/arquivos/ids/200732-800-800/767022_ampliada.jpg?v=637266030488030000"
   },
   {
- id: 3,
+    id: 3,
     name: "Pequeno Manual Antirracista",
     author: "Djamila Ribeiro",
     pages: 136,
-    image:"https://images-na.ssl-images-amazon.com/images/I/71D3cAdm0GL.jpg"
-  }, 
+    image: "https://images-na.ssl-images-amazon.com/images/I/71D3cAdm0GL.jpg"
+  },
   {
- id: 4,
+    id: 4,
     name: "Homofobia - História e crítica de um preconceito",
     author: "Daniel Borrillo",
     pages: 144,
-    image:"https://images-na.ssl-images-amazon.com/images/I/91AzPtbpRuL.jpg"
-  }  
+    image: "https://images-na.ssl-images-amazon.com/images/I/91AzPtbpRuL.jpg"
+  },
+  {
+    id: 5,
+    name: "Amazônia Indígena",
+    author: "Márcio Souza",
+    pages: 256,
+    image: "https://m.media-amazon.com/images/I/71fhfi5BodL.jpg"
+  }
 ];
